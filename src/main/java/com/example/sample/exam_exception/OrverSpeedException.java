6eeeec4b15307a1feab179471a574cd7175bf321
igamasayuki <igamasayuki@gmail.com>
2025-04-24 17:06:48 +0900

    例外サンプル修正

--- src/main/java/com/example/sample/exam_exception/OrverSpeedException.java
+++ src/main/java/com/example/sample/exam_exception/OrverSpeedException.java
@@ -2,8 +2,6 @@ package com.example.sample.exam_exception;
 
 public class OrverSpeedException extends Exception {
 
-	private static final long serialVersionUID = 1L;
-
 	public OrverSpeedException() {
 	}
 
@@ -17,6 +15,11 @@ public class OrverSpeedException extends Exception {
 
 	public OrverSpeedException(String message, Throwable cause) {
 		super(message, cause);
+	}
+
+	public OrverSpeedException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
 	}
 
 }
